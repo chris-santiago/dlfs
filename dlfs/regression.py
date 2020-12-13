0b78cae4ffa4d1295018949847b657907ebbb9b8
cjsantiago <cjsantiago@gatech.edu>
2020-12-13 15:37:42 -0500

    add nn regressor

--- dlfs/regression.py
+++ dlfs/regression.py
@@ -2,6 +2,9 @@ from typing import Dict, Tuple, Optional, Union, List
 
 import numpy as np
 
+from dlfs.base import sigmoid
+from dlfs.metrics import mse
+
 Batch = Tuple[np.ndarray, np.ndarray]
 
 
@@ -52,24 +55,6 @@ class LinearRegressor:
         """Initialize weights for first forward pass."""
         self._weights["W"] = np.random.randn(n_weights, 1)
         self._weights["B"] = np.random.randn(1, 1)
-        return self
-
-    def _foward_pass(
-        self, x_batch: np.ndarray, y_batch: np.ndarray
-    ) -> "LinearRegressor":
-        """Compute forward pass for linear regression."""
-        assert x_batch.shape[0] == y_batch.shape[0]
-        assert x_batch.shape[1] == self._weights["W"].shape[0]
-        assert self._weights["B"].shape[0] == self._weights["B"].shape[1] == 1
-
-        N = np.dot(x_batch, self._weights["W"])
-        P = N + self._weights["B"]
-        self.loss = np.mean(np.power(y_batch - P, 2))
-
-        self._forward_info["X"] = x_batch
-        self._forward_info["N"] = N
-        self._forward_info["P"] = P
-        self._forward_info["y"] = y_batch
         return self
 
     def _get_forward_loss(self, x_batch: np.ndarray, y_batch: np.ndarray) -> float:
@@ -130,3 +115,123 @@ class LinearRegressor:
     def predict(self, X: np.ndarray) -> np.ndarray:
         """Generate predictions from regression model."""
         return np.dot(X, self._weights["W"]) + self._weights["B"]
+
+
+class NNRegressor(LinearRegressor):
+    """Class for manual neural network regressor."""
+
+    def __init__(
+            self,
+            hidden_size: int,
+            learning_rate: float = 0.01,
+            batch_size: Optional[int] = 100,
+            n_iter: int = 1000,
+            validate_step: int = 1000,
+            return_losses: bool = False,
+            return_weights: bool = False,
+            return_scores: bool = False,
+            seed: int = 1,
+    ):
+        self.hidden_size = hidden_size
+        self.validate_step = validate_step
+        self._return_scores = return_scores
+        super().__init__(learning_rate, batch_size, n_iter, return_losses, return_weights, seed)
+
+    def _initialize_weights(self, input_size: int) -> "NNRegressor":
+        """Initialize weights for first forward pass."""
+        self._weights["W1"] = np.random.randn(input_size, self.hidden_size)
+        self._weights["B1"] = np.random.randn(1, self.hidden_size)
+        self._weights["W2"] = np.random.randn(self.hidden_size, 1)
+        self._weights["B2"] = np.random.randn(1, 1)
+        return self
+
+    def _get_forward_loss(self, x_batch: np.ndarray, y_batch: np.ndarray) -> float:
+        """Generate fitted values and compute loss. """
+        input_fit_no_bias = np.dot(x_batch, self._weights["W1"])
+        input_fit_with_bias = input_fit_no_bias + self._weights["B1"]
+        activation_fit = sigmoid(input_fit_with_bias)
+        hidden_fit_no_bias = np.dot(activation_fit, self._weights['W2'])
+        hidden_fit_with_bias = hidden_fit_no_bias + self._weights['B2']
+        self.loss = float(np.mean(np.power(y_batch - hidden_fit_with_bias, 2)))
+        self._forward_info["X"] = x_batch
+        self._forward_info["M1"] = input_fit_no_bias
+        self._forward_info["N1"] = input_fit_with_bias
+        self._forward_info["O1"] = activation_fit
+        self._forward_info["M2"] = hidden_fit_no_bias
+        self._forward_info["P"] = hidden_fit_with_bias
+        self._forward_info["y"] = y_batch
+        return self.loss
+
+    def _get_loss_gradients(self) -> "NNRegressor":
+        """Compute gradients for NN regression."""
+        dL_dP = -2 * (self._forward_info["y"] - self._forward_info["P"])
+        dP_dM2 = np.ones_like(self._forward_info['M2'])
+        dL_dM2 = dL_dP * dP_dM2
+        dP_dB2 = np.ones_like(self._weights['B2'])
+        dL_dB2 = (dL_dP * dP_dB2).sum(axis=0)
+
+        dM2_dW2 = np.transpose(self._forward_info['O1'], (1, 0))
+        dL_dW2 = np.dot(dM2_dW2, dL_dP)
+
+        dM2_dO1 = np.transpose(self._weights['W2'], (1, 0))
+        dL_dO1 = np.dot(dL_dM2, dM2_dO1)
+        dO1_dN1 = sigmoid(self._forward_info['N1']) * (1 - sigmoid(self._forward_info['N1']))
+        dL_dN1 = dL_dO1 * dO1_dN1
+        dN1_dB1 = np.ones_like(self._weights['B1'])
+        dN1_dM1 = np.ones_like(self._forward_info['M1'])
+        dL_dB1 = (dL_dN1 * dN1_dB1).sum(axis=0)
+
+        dL_dM1 = dL_dN1 * dN1_dM1
+        dM1_dW1 = np.transpose(self._forward_info['X'], (1, 0))
+        dL_dW1 = np.dot(dM1_dW1, dL_dM1)
+
+        self._loss_gradients["W2"] = dL_dW2
+        self._loss_gradients["B2"] = dL_dB2.sum(axis=0)
+        self._loss_gradients["W1"] = dL_dW1
+        self._loss_gradients["B1"] = dL_dB1.sum(axis=0)
+        return self
+
+    def fit(
+            self, X: np.ndarray, y: np.ndarray, x_valid: np.ndarray, y_valid: np.ndarray
+    ) -> Union[
+        Tuple[List[float], Dict[str, np.ndarray], List[float]], List[float], Tuple[List[float], List[float]], "LinearRegressor"
+    ]:
+        """Fit regression model."""
+        if self._seed:
+            np.random.seed(self._seed)
+        start = 0
+        self._initialize_weights(X.shape[1])
+        X, y = self._permute_data(X, y)
+        losses = []
+        val_scores = []
+        for i in range(self._n_iter):
+            if start >= X.shape[0]:
+                X, y = self._permute_data(X, y)
+                start = 0
+            x_batch, y_batch = self._make_batch(X, y, start)
+            start += self._batch_size
+            self.loss = self._get_forward_loss(x_batch, y_batch)
+            if self._return_losses:
+                losses.append(self.loss)
+            self._get_loss_gradients()
+            for param in self._weights:
+                self._weights[param] -= self._eta * self._loss_gradients[param]
+            if self._return_scores:
+                if (i % self.validate_step == 0) and (i != 0):
+                    preds = self.predict(x_valid)
+                    val_scores.append(float(mse(y_valid, preds)))
+        if self._return_weights:
+            return losses, self._weights, val_scores
+        if self._return_scores:
+            return losses, val_scores
+        if self._return_losses:
+            return losses
+        return self
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Generate predictions from the step-by-step neural network model."""
+        M1 = np.dot(X, self._weights['W1'])
+        N1 = M1 + self._weights['B1']
+        O1 = sigmoid(N1)
+        M2 = np.dot(O1, self._weights['W2'])
+        return M2 + self._weights['B2']
