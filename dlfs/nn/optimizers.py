c80ed7e43f2338bffc0e445acc628c494c73b5fa
cjsantiago <cjsantiago@gatech.edu>
2020-12-14 22:40:43 -0500

    add sgd momentum optim

--- dlfs/nn/optimizers.py
+++ dlfs/nn/optimizers.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Optimizer:
     """Base class for a neural network optimizer."""
 
@@ -28,3 +31,40 @@ class SGD(Optimizer):
                 param -= self.lr * param_grad
         else:
             raise AttributeError("Net attribute cannot be empty.")
+
+
+class SGDMomentum(Optimizer):
+    """Stochastic gradient descent optimizer with momentum."""
+    def __init__(self,
+                 lr: float = 0.01,
+                 final_lr: float = 0,
+                 decay_type: str = None,
+                 momentum: float = 0.9):
+        """Constructor method."""
+        super().__init__(lr)
+        self.final_lr = final_lr
+        self.decay_type = decay_type
+        self.momentum = momentum
+        self.velocities = None
+        self.first = None
+        self.net = None
+
+    def step(self) -> None:
+        """
+        For each parameter, adjust in the appropriate direction, with the magnitude of the
+        adjustment based on the learning rate.
+        """
+        if self.first:
+            self.velocities = [np.zeros_like(param) for param in self.net.params()]
+            self.first = False
+
+        for (param, param_grad, velocity) in zip(self.net.params(),
+                                                 self.net.param_grads(),
+                                                 self.velocities):
+            self._update_rule(param=param, grad=param_grad, velocity=velocity)
+
+    def _update_rule(self, **kwargs) -> None:
+        """Update velocity and parameters."""
+        kwargs['velocity'] *= self.momentum
+        kwargs['velocity'] += self.lr * kwargs['grad']
+        kwargs['param'] -= kwargs['velocity']
--- dlfs/nn/utils.py
+++ dlfs/nn/utils.py
@@ -1,4 +1,6 @@
 from numpy import ndarray
+import numpy as np
+from scipy.special import logsumexp
 
 
 def assert_same_shape(array_1: ndarray, array_2: ndarray):
@@ -10,3 +12,19 @@ def assert_same_shape(array_1: ndarray, array_2: ndarray):
         """
     assert array_1.shape == array_2.shape, msg
     return None
+
+
+def softmax(x, axis=None):
+    """Softmax function."""
+    return np.exp(x - logsumexp(x, axis=axis, keepdims=True))
+
+
+# def normalize(a: np.ndarray):
+#     """Normalize an array."""
+#     other = 1 - a
+#     return np.concatenate([a, other], axis=1)
+#
+#
+# def unnormalize(a: np.ndarray):
+#     """Un-Normalize an array."""
+#     return a[np.newaxis, 0]
